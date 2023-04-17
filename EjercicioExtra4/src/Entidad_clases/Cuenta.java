f4392aed18d04665635c92d27d0e5aa7f1172897
Nani Bertini <bertinidaniela75@gmail.com>
2023-04-17 19:43:57 -0300

    primer commit

---
+++ EjercicioExtra4/src/Entidad_clases/Cuenta.java
@@ -0,0 +1,14 @@
+/*
+ Crea una clase "Cuenta" que tenga atributos como "saldo" y "titular". Luego, 
+crea un método "retirar_dinero" que permita retirar una cantidad de dinero del
+saldo de la cuenta. Asegúrate de que el saldo nunca sea negativo después de 
+realizar una transacción de retiro.
+
+ */
+package Entidad_clases;
+
+public class Cuenta {
+    public 
+    
+}
+//
